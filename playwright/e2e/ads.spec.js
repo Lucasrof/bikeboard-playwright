0dd1781a1a7c181053fa10bce0ab7706b2101bc3
Lucas Rosa <lucasrof7@gmail.com>
2025-12-13 23:05:01 -0300

    forçando o erro novamente, entendendo o action

--- playwright/e2e/ads.spec.js
+++ playwright/e2e/ads.spec.js
@@ -62,7 +62,7 @@ test.describe("Cadastro de Anúncio", () => {
     ).toBeVisible()
   })
 
-  test("text-WhatsApp deve conter menos de 10 digitos", async ({
+  test("text-WhatsApp deve ter menos de 10 digitos", async ({
     page
   }) => {
     await setupWhatsAppInvalidLengthMock(page)
@@ -70,7 +70,7 @@ test.describe("Cadastro de Anúncio", () => {
     await actions.submitAdForm()
 
     await expect(
-      page.locator("text=WhatsApp deve ter 10 ou 11 dígitos")
+      page.locator("text-WhatsApp deve ter menos de 10 digitos")
     ).toBeVisible()
   })
 
